a31e5eaa26b162874d58e3d840eefe4336308833
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2019-08-25 06:06:01 +0600

    Update example_003.py

--- python/example_003.py
+++ python/example_003.py
@@ -1,5 +1,6 @@
 import skimage.io as io
 
+
 def convertImageType():
     originalImage = io.imread('images/bowl_fruit.png')
     io.imsave('images/bowl_fruit.jpeg', originalImage)
@@ -7,6 +8,7 @@ def convertImageType():
     changedImage = io.imread('images/bowl_fruit.jpeg')
     io.imshow(changedImage)
     io.show()
+
 
 convertImageType()
 
